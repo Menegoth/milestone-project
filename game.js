a75585312f42812234930decc9cc9a953e309282
Menegoth <antonioareas@me.com>
2022-07-12 13:27:16 -0700

    display results and remove buttons

--- game.js
+++ game.js
@@ -1,46 +1,71 @@
-function startGame() {
+//create player hand array to add cards to
+let playerHand = [];
+createNewPlayerCard();
+createNewPlayerCard()
 
-    //create player hand array to add cards to
-    let playerHand = [];
-    playerHand.push(createNewPlayerCard());
-    playerHand.push(createNewPlayerCard());
-    
-    playerTurn();
+const hitButton = document.getElementById("hit");
+const stopButton = document.getElementById("stop");
+
+createButtonFunctionalities();
+
+//button functionalities
+function createButtonFunctionalities() {
+    hitButton.addEventListener("click", createNewPlayerCard);
+    stopButton.addEventListener("click", displayResults);
 }
 
 function createNewPlayerCard() {
 
+    //create new card object
     let card = new Card();
 
+    //create elements for each 
     const cardDiv = document.createElement("div");
     const cardNumber = document.createElement("h1");
     const cardSuit = document.createElement("p");
 
+    //div for card
     cardDiv.style.width = "120px";
     cardDiv.style.height = "175px";
     cardDiv.style.border = "1px solid black";
     cardDiv.style.display = "inline-block";
 
+    //set values
     cardSuit.textContent = card.suit;
     cardNumber.textContent = card.number;
 
+    //append all elements to page
     cardDiv.append(cardSuit, cardNumber);
 
     document.getElementById("player-hand").append(cardDiv);
 
-    return card;
+    playerHand.push(card);
 
 }
 
-function playerTurn() {
+//check total of values in hand
+function checkScore(hand) {
+    let total = 0;
+    hand.forEach(card => {
+        total += card.value;
+    })
+    return total;
+}
 
-    let playing = true;
-    while (playing) {
+//check results
+function displayResults() {
+    //remove button functionality
+    hitButton.removeEventListener("click", createNewPlayerCard);
+    stopButton.removeEventListener("click", displayResults);
 
-        
+    const results = document.getElementById("results-header");
+    let playerScore = checkScore(playerHand);
 
+    if (playerScore < 21) {
+        results.textContent = "less than 21 but still win"
+    } else if (playerScore == 21) {
+        results.textContent = "you win";
+    } else {
+        results.textContent = "you lose"
     }
-
 }
-
-startGame();
