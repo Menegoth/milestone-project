3cfac65d4d70551cce4ce85a6b390512185c9f20
Menegoth <antonioareas@me.com>
2022-07-13 19:16:07 -0700

    replay button

--- game.js
+++ game.js
@@ -1,25 +1,29 @@
-//create player hand array to add cards to
+//dom elements
+const resultsHeader = document.getElementById("results-header");
 const playerHandDiv = document.getElementById("player-hand");
-let playerHand = [];
-createNewPlayerCard();
-createNewPlayerCard();
-
-//create computer hand 
-let computerHand = [];
 const computerHandDiv = document.getElementById("computer-hand");
-
-//player logic
 
 const hitButton = document.getElementById("hit");
 const stopButton = document.getElementById("stop");
+const replayButton = document.getElementById("replay");
+
+//hand arrays
+let playerHand = [];
+let computerHand = [];
 
 createButtonFunctionalities();
+
+//add cards to player array
+createNewPlayerCard();
+createNewPlayerCard();
 
 //button functionalities
 function createButtonFunctionalities() {
     hitButton.addEventListener("click", createNewPlayerCard);
     stopButton.addEventListener("click", computerTurn); 
 }
+
+//player logic
 
 function createCard(hand, handDiv) {
 
@@ -88,52 +92,79 @@ function computerTurn() {
 
     }
 
-    console.log(computerHand, checkScore(computerHand)); //testing
     displayResults();
 }
 
 
 //check results
 function displayResults() { 
-    const resultsHeader = document.getElementById("results-header");
     let playerScore = checkScore(playerHand);
     let computerScore = checkScore(computerHand);
 
     if (playerScore > computerScore && playerScore <= 21) { //player has higher number while still being below or equal to 21
-        resultsHeader.textContent = "You Win";
+        resultsHeader.textContent = "Result: You Win";
     } else if (computerScore > playerScore && computerScore <= 21) { //computer has higher number while still below or equal to 21
-        resultsHeader.textContent = "Computer Wins";
+        resultsHeader.textContent = "Result: Computer Wins";
     } else if (playerScore > 21 && computerScore <= 21) { //player goes over 21 computer does not
-        resultsHeader.textContent = "Computer Wins";
+        resultsHeader.textContent = "Result: Computer Wins";
     } else if (computerScore > 21 && playerScore <= 21) { //computer goes over 21 and player does not
-        resultsHeader.textContent = "Player Wins";
+        resultsHeader.textContent = "Result: Player Wins";
     } else {
-        resultsHeader.textContent = "Tie Game";
+        resultsHeader.textContent = "Result: Tie Game";
     }
+
+    console.log(playerScore, computerScore) //for reference
+
+    //make replay button work
+    replayButton.addEventListener("click", restartGame);
     
 }
 
-/* OUTCOMES
+//function to restart game
+function restartGame() {
 
-    player has higher number while below or equal 21
-    computer has higher number while below below or equal 21
-    player goes over 21 computer does not
-    computer goes over 21 player does not
-    anything else is a tie
+    //reset both hands
+    playerHand = [];
+    computerHand = [];
 
+    //edit dom
+    playerHandDiv.innerHTML = "";
+    computerHandDiv.innerHTML = "";
+    resultsHeader.textContent = "Result: "
+
+    //add and remove buttons
+    createButtonFunctionalities()
+    replayButton.removeEventListener("click", restartGame);
+
+    //add two cards to player hand
+    createNewPlayerCard();
+    createNewPlayerCard();
+
+    console.log("replayed")
+}
+
+/* RESTART:
+    reset both hands and empty hand divs
+    re-add functionality to hit and stop buttons
+    profit?
 */
+
 
 /*TODO:
 
     add computer logic (done)
     add score checking logic (done)
 
-    add replay button
+    add replay button (done)
     scorecounter
     save score locally
 
     after game works:
         add images for each card
         style elements
+            buttons grayed out when not available
+            background
+            text
+            (likely bootstrap)
 
 */
